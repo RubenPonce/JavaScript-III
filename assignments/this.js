effcdbb9465f5abce31876d8e5f4635e452f5f60
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-03 12:55:24 -0700

    finished this.js assignment

--- assignments/this.js
+++ assignments/this.js
@@ -14,24 +14,45 @@
 function windowBinder(params) {
     return this;
 }
-console.log(windowBinder());
+// console.log(windowBinder());
 // Principle 2
 // code example for Implicit Binding
 const implicitBinding = {
     "name": 'Ruben',
-    "schoolt": "Lambda School",
+    "school": "Lambda School",
     "bio": function (){
         return `${this.name} is a student at ${this.school}`;
     }
 
 }
-console.log(implicitBinding.bio());
+// console.log(implicitBinding.bio());
 
 
 // Principle 3
 
 // code example for New Binding
-
+function Binding(name){
+    this.name = name;
+    this.school = 'Lambda School'
+    this.bio = function () {
+        return `${this.name} is a student at ${this.school}`;
+    }
+}
+const steven = new Binding ('Steven');
+// console.log(steven.bio()); 
 // Principle 4
 
 // code example for Explicit Binding
+function Student(name, school) {
+    this.name = name;
+    this.school = school;
+
+}
+function Occupation(name, school, occupation){
+    Student.call(this, name, school);
+    this.occupation = occupation;
+    this.introduction = function (){
+        return `${this.name} is a ${this.occupation} that went to ${this.school}`
+    }
+}
+// console.log(new Occupation('Ruben', 'Lambda School', 'Full Stack Developer').introduction())
