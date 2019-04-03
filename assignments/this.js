f0551ccaab3f5d335425e0758ba080cdf8f4a087
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-03 12:35:33 -0700

    first commit

--- assignments/this.js
+++ assignments/this.js
@@ -10,12 +10,23 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
-
+function windowBinder(params) {
+    return this;
+}
+console.log(windowBinder());
 // Principle 2
-
 // code example for Implicit Binding
+const implicitBinding = {
+    "name": 'Ruben',
+    "schoolt": "Lambda School",
+    "bio": function (){
+        return `${this.name} is a student at ${this.school}`;
+    }
+
+}
+console.log(implicitBinding.bio());
+
 
 // Principle 3
 
